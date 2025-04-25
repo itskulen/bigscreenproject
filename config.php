b4c69d06b383dfa7dec997074320864eda377e90
Wahyu Oktavian <wahyur2bro@gmail.com>
2025-04-25 10:44:14 +0700

    add .gitignore

--- config.php
+++ config.php
@@ -10,6 +10,7 @@ $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
+    // PDO::ATTR_PERSISTENT         => true, // Persistent connection
 ];
 
 try {
